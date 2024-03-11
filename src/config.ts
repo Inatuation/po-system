3d81d2f1a06e11b1fe5bd1881c4a68dbaafcacc4
Infatuation <5252107+infatuation5858@user.noreply.gitee.com>
2024-03-11 23:08:31 +0800

    commit: config change

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const __POWERED_BY_WUJIE__ = false; // 是否启用无界
+export const __POWERED_BY_WUJIE__ = true; // 是否启用无界
 
 export const logoUrl: any = {
 	music: new URL('@/assets/desktop/music.png', import.meta.url).href,
@@ -24,7 +24,7 @@ export const wujieAppList: any = {
 		alive: true,
 		sync: true,
 		name: 'chat',
-		url: 'http://localhost:8099',
+		url: 'http://112.74.49.183:5716',
 		defaultWidth: 1000,
 		defaultHeight: 800,
 		customHeader: true,
